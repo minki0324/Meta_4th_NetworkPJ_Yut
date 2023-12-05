ca32c68fcc1756285cefa92568f44fdcedfff80d
고승현 <rhtmdgus4190@naver.com>
2023-12-05 15:09:34 +0900

    [Feat] UI
    UI제작중..

---
+++ YutNole/Assets/3.Script/Canvas_Net.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Canvas_Net : MonoBehaviour
+{
+
+    [SerializeField]
+    private Slider P1_Timer_slider;
+
+    [SerializeField]
+    private Slider P2_Timer_slider;
+
+
+    //던진 결과 
+    [SerializeField]
+    private Image[] Result_imgs;
+
+    [SerializeField]
+    private Sprite[] Result_sprites;
+
+
+    //플레이어 말
+    [SerializeField]
+    private GameObject[] P1_Unit;
+
+    [SerializeField]
+    private GameObject[] P2_Unit;
+
+
+    private void Awake()
+    {
+    
+    }
+
+    private void Start()
+    {
+
+      
+    }
+  
+    
+   
+
+
+
+}
+    
---
+++ YutNole/Assets/3.Script/ThrowYut_Button.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ThrowYut_Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private Button ThrowYut_Btn;
+
+    [SerializeField]
+    private Sprite[] ThrowYut_sprites;
+    /*
+     0. Disable
+    1. Idle
+    2. Mouse Over
+    3. Press
+     */
+
+
+    bool isMouseOn;
+
+    void Start()
+    {
+        ThrowYut_Btn. GetComponent<Button>();
+
+        ThrowYut_Btn.onClick.AddListener(ThrowYut_Btn_Clicked);
+        ThrowYut_Btn.GetComponent<Image>().sprite = ThrowYut_sprites[0];
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ThrowYut_Btn.GetComponent<Image>().sprite = ThrowYut_sprites[2];
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ThrowYut_Btn.GetComponent<Image>().sprite = ThrowYut_sprites[1];
+        throw new System.NotImplementedException();
+    }
+
+   
+  
+    public void ThrowYut_Btn_Clicked()
+    {
+        ThrowYut_Btn.GetComponent<Image>().sprite = ThrowYut_sprites[3];
+        Debug.Log("¤»¤»");
+    }
+
+}
