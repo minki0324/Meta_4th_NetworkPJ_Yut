470092833a816bece7561ae69db292c715e6cd1b
minki0324 <147981201+minki0324@users.noreply.github.com>
2023-12-05 15:08:58 +0900

    [Feat] Yut Anim

---
+++ YutNole/Assets/3.Script/Yut_Gacha.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yut_Gacha : MonoBehaviour
+{
+    /*
+        1. 확률에 따라서 윷 애니메이션 출력
+    */
+
+    private Animator Yut_ani;
+
+    private void Awake()
+    {
+        Yut_ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            string[] triggers = { "Do", "Do", "Do", "Backdo", "Gae", "Gae", "Gae", "Gae", "Gae", "Gae", "Geol", "Geol", "Geol", "Geol", "Yut", "Mo", "Nack", "Nack" };
+            Yut_ani.SetTrigger(triggers[Random.Range(0, triggers.Length)]);
+        }
+    }
+}
