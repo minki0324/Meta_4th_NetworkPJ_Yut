77f097914ba20f0078e3b0f580f0f58c25690f52
minki0324 <147981201+minki0324@users.noreply.github.com>
2023-12-09 14:22:00 +0900

    [Feat] Yut Result Add, Del
    윷놀이 결과값에 대한 딱지 출력 및 제거 로직 서버 동기화 완료

--- YutNole/Assets/3.Script/Network/Server_Manager.cs
+++ YutNole/Assets/3.Script/Network/Server_Manager.cs
@@ -8,36 +8,17 @@ public class Server_Manager : NetworkBehaviour
     public static Server_Manager instance;
 
     #region Script
-    [SerializeField] private Yut_Gacha Yut_Ani;
     #endregion
 
     #region SyncVar
     [SyncVar(hook = nameof(OnTurn_Finish))] 
     public int Turn_Index = 2;
-
-    [SyncVar]
-    public string ThrowResult = string.Empty;
     #endregion
 
     #region Client
-    [Client]
-    public void Yut_Btn_Click()
-    {
-
-    }
     #endregion
 
     #region Command
-    // 윷 던지는 애니메이션 CMD
-    [Command(requiresAuthority = false)]
-    private void CMDYut_Throwing()
-    {
-        string[] triggers = { "Do", "Do", "Do", "Backdo", "Gae", "Gae", "Gae", "Gae", "Gae", "Gae", "Geol", "Geol", "Geol", "Geol", "Yut", "Mo", "Nack", "Nack" };
-        ThrowResult = triggers[Random.Range(0, triggers.Length)];
-
-        RPCYut_Throwing(ThrowResult);
-    }
-
     // 턴 바꾸는 CMD
     [Command(requiresAuthority = false)]
     public void CMD_Turn_Changer()
@@ -45,18 +26,9 @@ public class Server_Manager : NetworkBehaviour
         int next_Index = (Turn_Index % 2) + 1;
         OnTurn_Finish(Turn_Index, next_Index);
     }
-
-    
     #endregion
 
     #region ClientRPC
-    // 애니메이션 출력 RPC
-    [ClientRpc]
-    private void RPCYut_Throwing(string trigger)
-    {
-        Debug.Log("RpcRPCYut_Throwing 호출됨");
-        Yut_Ani.Throwing(trigger);
-    }
     #endregion
 
     #region Unity Callback
